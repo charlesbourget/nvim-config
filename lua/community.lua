7fb6e32e99866320e212a838d4fc3b2ca699c2f9
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2024-04-13 22:38:40 -0400

    Clean up config and add custom key maps

--- lua/community.lua
+++ lua/community.lua
@@ -4,7 +4,15 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- color scheme
   { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- ai assistant
   { import = "astrocommunity.completion.codeium-vim" },
+
+  -- zig lsp support
   { import = "astrocommunity.pack.zig" },
+
+  -- live markdown preview
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 }
